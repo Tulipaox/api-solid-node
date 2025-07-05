a3ebd91406afa2bde57ce7c8adb04290b0ae48be
Filipe Lima <filipe.queiroz21@gmail.com>
2025-07-04 23:58:01 -0300

    feat: adicionando caso de uso para procurar academia pelo nome

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,26 @@
+import { Gym } from "@prisma/client";
+import { GymRepository } from "@/repositories/prisma/gyms-repository";
+
+interface SearchGymsUseCaseRequest {
+  query: string;
+  page: number;
+}
+
+interface SearchGymsCaseResponse {
+  gyms: Gym[];
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page);
+
+    return {
+      gyms,
+    };
+  }
+}
