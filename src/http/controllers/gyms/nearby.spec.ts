bd54d374b5972c90ca058faa5a5b3df463ccdb24
Filipe Lima <filipe.queiroz21@gmail.com>
2025-07-12 23:43:55 -0300

    feat(api): criação de controllers e testes e2e para gyms, check-ins e users

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,57 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe(" Nearby Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able list nearby gyms", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "JavaScript Gym",
+        description: "Some description.",
+        phone: "1199999999",
+        latitude: -23.7174784,
+        longitude: -46.8877312,
+      });
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "TypeScript Gym",
+        description: "Some description.",
+        phone: "1199999999",
+        latitude: -23.7513255,
+        longitude: -46.687143,
+      });
+
+    const response = await request(app.server)
+      .get("/gyms/nearby")
+      .query({
+        latitude: -23.7174784,
+        longitude: -46.8877312,
+      })
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: "JavaScript Gym",
+      }),
+    ]);
+  });
+});
