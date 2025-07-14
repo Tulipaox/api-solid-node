0cac59cfe966c91227aac8ad656b3b41d9c3eae8
Filipe Lima <filipe.queiroz21@gmail.com>
2025-07-14 15:07:32 -0300

    retirando github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,27 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    exclude: ["build/**", "node_modules/**"],
     include: ["src/http/controllers/**/*.spec.ts"],
-    exclude: ["prisma/**", "node_modules/**", "build/**"],
-    environment: "prisma",
+    name: "prisma",
+    projects: [
+      {
+        extends: true,
+        test: {
+          environment: "prisma",
+          exclude: ["prisma/**"],
+        },
+      },
+    ],
+  },
+
+  optimizeDeps: {
+    exclude: ["prisma"],
+  },
+
+  build: {
+    rollupOptions: {
+      external: ["prisma"],
+    },
   },
 });
